1947e2a44db8ef5b1908cfaf53de62d276a4a092
Jonah <jonahs99@gmail.com>
2018-01-10 13:33:29 -0500

    blur works maybe?

--- imageloader.java
+++ imageloader.java
@@ -7,14 +7,13 @@ import java.io.IOException;
 import java.io.File;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
-import org.apache.commons.math3.analysis.function.Gaussian;
 
 public class imageloader
 {
   public static void main(String[] args)
   {
     int[][][] img = imageloader.load("notlimegReEn.jpg");
-    int[][][] blurred = imageloader.gaussianBlur(img, 3);
+    int[][][] blurred = imageloader.gaussianBlur(img, 10);
   }
 
   public static int[][][] load(String path) {
@@ -47,7 +46,7 @@ public class imageloader
   }
 
   private static double gauss(double x) {
-    sigma2 = 1.0;
+    double sigma2 = 1.0;
     return 1.0 / Math.sqrt(2 * Math.PI * sigma2) * 
       Math.exp(-Math.pow(x, 2) / 2.0 / sigma2);
   }
@@ -55,20 +54,51 @@ public class imageloader
   private static double[][] gaussianRadius(int radius){
     int mid = radius + 1;
     double[][] val = new double[2*radius + 1][2*radius + 1];
+
     for(int k = 0; k < 2 * radius + 1;k++){
       for(int n = 0; n < 2 * radius + 1;n++){
         double dist = Math.sqrt(Math.pow(k-mid,2) + Math.pow(n-mid,2));
         val[k][n] = gauss(dist);
+        if (dist > radius){
+          val[k][n] = 0;
+        }
       }
-    
     }
-
-
+    return val;
   }
 
   private static int[][][] gaussianBlur(int[][][] image, int radius) {
-    for(int k = 0)
-    return null;
+    int[][][] blurredImage = new int[image.length][image[0].length][3];
+    double[][] weights = gaussianRadius(radius);
+    for(int k = 0; k < image.length; k++) {
+      for (int n = 0; n < image[0].length; n++) {
+        double r = 0;
+        double g = 0;
+        double b = 0;
+        double weightSum = 0;
+        for (int i = 0; i < radius * 2 + 1; i++) {
+          for (int j = 0; j < radius * 2 + 1; j++) {
+            int col = k + i - radius;
+            int row = n + j - radius;
+            if (0 <= col && col < image.length) {
+              if (0 <= row && row < image[0].length) {
+                r += image[col][row][0] * weights[i][j];
+                g += image[col][row][1] * weights[i][j];
+                b += image[col][row][2] * weights[i][j];
+                weightSum += weights[i][j];
+              }
+            }
+          }
+        }
+        r /= weightSum;
+        g /= weightSum;
+        b /= weightSum;
+        blurredImage[k][n][0] = (int) r;
+        blurredImage[k][n][1] = (int) g;
+        blurredImage[k][n][2] = (int) b;
+      }
+    }
+    return blurredImage;
   }
 
 }
