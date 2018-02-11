9d5e75888838133f0b3dbc7a7d01a09223b16f24
Jonah Simpson <jonahs99@gmail.com>
2018-02-10 20:55:21 -0500

    Add an undo with right click

--- gui/window.java
+++ gui/window.java
@@ -1,15 +1,16 @@
 package gui;
+
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 
 import java.awt.Point;
 import java.util.ArrayList;
 
-public class window extends Frame
-{
+public class window extends Frame {
 
   ImagePanel pan;
   public ArrayList<Point> selection = new ArrayList();
@@ -27,20 +28,34 @@ public class window extends Frame
     pan.repaint();
   }
 
-  private class MouseEventHandler extends MouseAdapter
-  {
-    window W;
-    public MouseEventHandler(window wndw){
-      this.W = wndw;
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
-      
-      System.out.println("We got clicked!");
-      this.W.selectPoint(e.getPoint());
-      //this.W.BlurredImage();
-    }
+  public void undoSelect() {
+    if (selection.size() == 0)
+      return;
+    selection.remove(selection.size() - 1);
+    pan.repaint();
   }
 
+  private class MouseEventHandler extends MouseAdapter {
+    window W;
+
+    public MouseEventHandler(window wndw) {
+      this.W = wndw;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+      switch (e.getModifiers()) {
+      case InputEvent.BUTTON1_MASK: // left
+        System.out.println("We got clicked!");
+        this.W.selectPoint(e.getPoint());
+        break;
+      case InputEvent.BUTTON3_MASK: // right
+        this.W.undoSelect();
+        break;
+      }
+
+    }
+  }
 
 }
